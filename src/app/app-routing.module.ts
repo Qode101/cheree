c7edd6d16f085a32740c25d4f71423edce902a4f
Bsakwa <sakwabrian23@gmail.com>
2024-11-19 11:02:10 +0300

    Added the landing page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { LoginComponent } from './auth/login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/sign-up', pathMatch: 'full' } // Redirect to sign-up by default
