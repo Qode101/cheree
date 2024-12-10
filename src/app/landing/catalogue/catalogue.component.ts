b265d3c2d5142bf3945203bc884713d27bf2f398
Bsakwa <sakwabrian23@gmail.com>
2024-12-10 12:35:54 +0300

    Added a shopping cart button to the card

--- src/app/landing/catalogue/catalogue.component.ts
+++ src/app/landing/catalogue/catalogue.component.ts
@@ -1,11 +1,13 @@
 // src/app/catalogue/catalogue.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './catalogue.component.html',  // Refer to the external HTML template
   styleUrls: ['./catalogue.component.css']   // Refer to the external CSS styles
 })
