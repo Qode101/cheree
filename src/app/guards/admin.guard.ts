3872fb5f52feb36987044cced9eb9606c78871e8
codelord-evans <ekioko404@gmail.com>
2024-12-16 11:36:48 +0300

    Refactored admin guard to redirect unauthorized users

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,5 +1,21 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../../app/auth.service';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    // Check if the user has admin privileges
+    if (this.authService.isAdmin()) {
+      return true; // Allow access
+    } else {
+      // Redirect to login or unauthorized page if not an admin
+      this.router.navigate(['/unauthorized']);
+      return false;
+    }
+  }
+}
